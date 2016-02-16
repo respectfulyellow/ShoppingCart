f44a6f0f53d166c9a93fad49983c60ade405b406
Eric Gurney <ekgurney14@gmail.com>
2016-02-16 16:53:46 -0500

    Added functional tests from kata docs

---
+++ ShoppingCart.FunctionalTests/FunctionTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Ninject;
+using NUnit.Framework;
+using Ninject.Extensions.Conventions;
+using ShoppingCart.Implementation;
+
+namespace ShoppingCart.FunctionalTests
+{
+    [TestFixture]
+    public class FunctionTests
+    {
+        private StandardKernel _kernel;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _kernel = new StandardKernel();
+            _kernel.Bind(x => x.FromAssemblyContaining<TotalCostCalculator>()
+                .SelectAllClasses()
+                .BindDefaultInterface().Configure(c => c.InSingletonScope()));
+            _kernel.Bind<IPricerDataService>().To<PricerDataService>()
+                .InSingletonScope();
+        }
+
+        private static IEnumerable TestData
+        {
+            get
+            {
+                yield return new TestCaseData("").Returns(0);
+                yield return new TestCaseData("A").Returns(50);
+                yield return new TestCaseData("AB").Returns(80);
+                yield return new TestCaseData("CDBA").Returns(115);
+                yield return new TestCaseData("AA").Returns(100);
+                yield return new TestCaseData("AAA").Returns(130);
+                yield return new TestCaseData("AAABB").Returns(175);
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestData))]
+        public int TestScenerio(string skus)
+        {
+            var totalCostCalculator = _kernel.Get<TotalCostCalculator>();
+            var totalCost = totalCostCalculator.Calculate(skus);
+
+            return totalCost;
+        }
+
+    }
+}
---
+++ ShoppingCart.FunctionalTests/PricerDataService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using ShoppingCart.Implementation;
+
+namespace ShoppingCart.FunctionalTests
+{
+    public class PricerDataService : IPricerDataService
+    {
+        public ICollection<IPricer> GetPricers()
+        {
+            return new List<IPricer>
+            {
+                new DiscountedItemPricer(50, 3, 130, 'A'),
+                new DiscountedItemPricer(30, 2, 45, 'B'),
+                new NonDiscountedPricer(20, 'C'),
+                new NonDiscountedPricer(15, 'D')
+            };
+        }
+    }
+}
