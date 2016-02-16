f6d60462872584b841be768c06115abed5bf8880
Eric Gurney <ekgurney14@gmail.com>
2016-02-16 16:55:32 -0500

    Cleaned up usings

--- ShoppingCart.FunctionalTests/FunctionTests.cs
+++ ShoppingCart.FunctionalTests/FunctionTests.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FluentAssertions;
+﻿using System.Collections;
 using Ninject;
 using NUnit.Framework;
 using Ninject.Extensions.Conventions;
